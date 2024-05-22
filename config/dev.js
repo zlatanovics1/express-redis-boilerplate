a314f0bbca002878bd93723051545fea74be5824
zlatanoviccs <zlatanovichs6@gmail.com>
2024-05-22 21:41:37 +0200

    Config file edited

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-  mongoURI:
-    "mongodb+srv://zlatanovichs6:ASyekLJmanVM7Uhe@advancednodeblogs.jlihxvm.mongodb.net/?retryWrites=true&w=majority&appName=advancednodeblogs",
-  cookieKey: "123123123",
-};
