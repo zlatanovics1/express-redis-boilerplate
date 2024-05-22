3fdda5580e6428c197bdcf1f7ad57bc5756c9afd
zlatanoviccs <zlatanovichs6@gmail.com>
2024-05-22 21:40:00 +0200

    Mongoose setup done

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    'mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority',
-  cookieKey: '123123123',
+    "mongodb+srv://zlatanovichs6:ASyekLJmanVM7Uhe@advancednodeblogs.jlihxvm.mongodb.net/?retryWrites=true&w=majority&appName=advancednodeblogs",
+  cookieKey: "123123123",
 };
