8f7351163c012cbf5011386f5750bc9b7095f8d2
zlatanoviccs <zlatanovichs6@gmail.com>
2024-05-31 19:11:41 +0200

    Landing page redesign

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>
-        Blogster!
-      </h1>
-      Write private blogs
+    <div style={{ textAlign: "center" }}>
+      <h1>Blogster!</h1>
+      Write your own private blogs
     </div>
   );
 };
